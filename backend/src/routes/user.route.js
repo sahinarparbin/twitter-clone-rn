3e6f712fce8645cb6a3f8d50b49393de449d8495
sahinaparbin <sahina.23ece@cambridge.edu.in>
2026-04-20 23:55:45 +0530

    fixed user routes and controller errors

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,10 +1,17 @@
 import express from "express";
-import { getUserProfile, updateProfile } from "../controllers/user.controller.js";
+import { getUserProfile, updateProfile, syncUser, getCurrentUser, followUser } from "../controllers/user.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
+// public route
 router.get("/profile/:username", getUserProfile);
+
+// protected routes
+router.post("/sync", protectRoute, syncUser);
+router.post("/me", protectRoute, getCurrentUser);
 router.put("/profile", protectRoute, updateProfile);
+router.post("/follow/:targetUserId", protectRoute, followUser);
+
 //update profile => auth
 export default router;
