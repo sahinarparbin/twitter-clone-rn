2df4614251894f7b9066e7a318c02350971598a3
sahinaparbin <sahina.23ece@cambridge.edu.in>
2026-04-23 02:11:30 +0530

    feat: update layout and index components with Tailwind styles and add nativewind type definitions

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -3,18 +3,12 @@ import  {Ionicons} from "@expo/vector-icons"
 
 export default function Index() {
   return (
-    <View style={ styles.container}>
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      <TextInput placeholder="Email address" />
-      <Ionicons name="logo-twitter" size={24} color="#007AFF" /> 
+     <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to Nativewind!
+      </Text>
     </View>
+
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
